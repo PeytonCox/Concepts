d39d9034ea716a7925a2109602c0a863db903a0b
BuildTools <ptc2m@mtmail.mtsu.edu>
2017-10-12 18:44:35 -0500

    Fixed commenting in main.cpp and updated README

--- Concept_001/Concept_001/main.cpp
+++ Concept_001/Concept_001/main.cpp
@@ -6,17 +6,19 @@ using namespace std;
 
 void Encrypt(string, string, string&);
 int KeyGen();
-int runCount = 1; //For debugging how many times the while loop has run
 string Decrypt(string, string);
+int runCount = 1; //For debugging how many times the while loop has run
+int debug = 0; //Controls debugging (0 = Off, 1 = On)
 
 int main() {
 
 	srand(time(NULL)); //Initialize the seed
 
-	string userInput;
-	string returnData;
-	string uCrypt;
-	string eKey = "";
+	//INITIALIZING VARIABLES IN MAIN()
+	string userInput; //string to be encrypted
+	string returnData; //Encrypted Data
+	string uCrypt; //Unencrypted Data
+	string eKey = ""; //Initialize encryption key
 
 	//This generates each individal number that then becomes eKey (The actual encryption key)
 	for (int i = 0; i < 24; i++) {
@@ -26,93 +28,123 @@ int main() {
 	//Print out the encryption key to later decrypt the string
 	cout << "Your encryption key, **KEEP THIS**:  " << eKey << endl;
 
+	//Takes in the input to be encrypted
 	cout << "Enter in a string to be 'encrypted':  ";
 	getline(cin, userInput);
 
-	cout << "-=ENCRYPTION BEGIN=-" << endl; //DEBUG
-	Encrypt(eKey, userInput, returnData);
-	cout << "-=DECRYPTION BEGIN=-" << endl;
-	uCrypt = Decrypt(eKey, returnData); //DEBUG
+	if (debug == 1) {
+		cout << "-=ENCRYPTION BEGIN=-" << endl; //DEBUG
+	}
+	Encrypt(eKey, userInput, returnData); //Calls function to encrypt the supplied string
+	if (debug == 1) {
+		cout << "-=DECRYPTION BEGIN=-" << endl; //DEBUG
+	}
+	uCrypt = Decrypt(eKey, returnData); //Calls function to decrypt the encrypted string
 
+	//Print out the encrypted and decrypted strings
 	cout << endl << "Here is your encryped string :  " << returnData << endl;
 	cout << endl << "Here is the unencrypted string: " << uCrypt << endl;
-	cin >> eKey;
-	return NULL;
+
+	cin >> eKey; //Basically pauses the program at the end, probably will replace
+	return NULL; //Main() function return
 }
 
 void Encrypt(string eKey, string data, string& returnData) {
 
-	char backLetter;
-	string eKeyBackup = eKey;
-	int currKey = NULL;
+	//INITIALIZING VARIABLES
+	char backLetter; //The back letter of the string to be encrypted
+	string eKeyBackup = eKey; //Backup of encryption key to replace when it's been completely run through
 
+	//Encryption loop, runs until entire supplied string has been run through
 	while (data.length() > 0) {
 
+		//if the encryption key has been used reset it here
 		if (eKey.length() == 0) {
 			eKey = eKeyBackup;
 		}
 
-		backLetter = data.back();
+		backLetter = data.back(); //Supply last character of string to backLetter
 
-		cout << "backLetter = " << backLetter << endl; //DEBUG
-		data.pop_back();
+		if (debug == 1) {
+			cout << "backLetter = " << backLetter << endl; //DEBUG
+		}
+		data.pop_back(); //Pop back of supplied string to allow for using data.back() next run
 
-		eKey.back() >> currKey;
-		backLetter = backLetter + eKey.back();
-		cout << "currKey = " << currKey << ", eKey.back() = " << eKey.back() << ", encryped letter = " << backLetter << ", data.length = " << data.length() << ", Run: " << runCount << endl; //DEBUG
-		eKey.pop_back();
-		runCount++; //Increase time ran by 1
+		backLetter = backLetter + eKey.back(); //Increase backLetter's value by the last value in eKey
+		if (debug == 1) {
+			cout << "eKey.back() = " << eKey.back() << ", encryped letter = " << backLetter << ", data.length = " << data.length() << ", Run: " << runCount << endl; //DEBUG
+		}
+		eKey.pop_back(); //Pop back of key to allow use of eKey.back() next run
+
+		if (debug == 1) {
+			runCount++; //Increase time ran by 1 (Used by debug)
+		}
 		
-		returnData += backLetter;
+		returnData += backLetter; //add encrypted character to string that will be called in main()
 
 	}
 
 }
 
+//Just randomly generates a number for the encryption key's generation
 int KeyGen() {
 
-	int key = NULL;
+	int key = NULL; //Initalize key
 
-	key = rand() % 9;
+	key = rand() % 9; //Generate random number from 0-9 for key
 
-	return key;
+	return key; //return value to be added to the end of eKey
 }
 
 string Decrypt(string eKey, string reverse) {
 
-	char backLetter;
-	string eKeyBackup = eKey;
-	string uCrypt;
+	//INITIALIZING VARIABLES
+	char backLetter; //The back letter of the string to be decrypted
+	string eKeyBackup = eKey; //Backup of encryption key to replace when it's been completely run through
+	string uCrypt; 
 	string uCrypted;
-	//string reverse;
-	int currKey = NULL;
 
 	//reset runCount to track decryption runs
 	runCount = 1;
 
+	//reverses the encrypted string so it can be decrypted appropriately
 	while (reverse.length() > 0){
 		uCrypt += reverse.back();
 		reverse.pop_back();
 	}
 
-
+	//Runs decryption until it has run all the way throught the encrypted string
 	while (uCrypt.length() > 0) {
 
+		//if the encryption key has been used reset it here
 		if (eKey.length() == 0) {
 			eKey = eKeyBackup;
 		}
-
+		
+		//supply the last letter of the encrypted string to backLetter
 		backLetter = uCrypt.back();
 
-		cout << "backLetter = " << backLetter << endl; //DEBUG
+		if (debug == 1) {
+			cout << "backLetter = " << backLetter << endl; //DEBUG
+		}
+		//pop back of uCrypt so uCrypt.back() can be used next run
 		uCrypt.pop_back();
 
-		eKey.back() >> currKey;
+		//Decrypt backLetter given the current value in eKey
 		backLetter = backLetter - eKey.back();
-		cout << "currKey = " << currKey << ", eKey.back() = " << eKey.back() << ", unencryped letter = " << backLetter << ", data.length = " << uCrypt.length() << ", Run: " << runCount << endl; //DEBUG
-		eKey.pop_back();
-		runCount++; //Increase time ran by 1
 
+		if (debug == 1) {
+			cout << "eKey.back() = " << eKey.back() << ", unencryped letter = " << backLetter << ", data.length = " << uCrypt.length() << ", Run: " << runCount << endl; //DEBUG
+		}
+
+		//pops the back of eKey so it can use the appropriate value next run
+		eKey.pop_back();
+
+		if (debug == 1) {
+			runCount++; //Increase time ran by 1 (Used by debug)
+		}
+
+		//add the decrypted character to the decrypted end product (will be backwards here)
 		uCrypted += backLetter;
 
 	}
